cec90314d5467f998ff80630b3884e2ec772f04d
zeekygeek <rajbck7@gmail.com>
2024-08-24 09:59:42 +0530

    initial-commit

--- WEEK 2/2.1/2.1 - Async.js
+++ WEEK 2/2.1/2.1 - Async.js
@@ -3,6 +3,8 @@
 //     console.log('Hi there')
 // }, 6000 )
 
+const { error } = require("console")
+
 function sayHello(name){
     console.log(`Hello ${name}`)
 }
@@ -14,3 +16,25 @@ function sayHello(name){
 //  sayHello('asi')
 // }, 6000)
     
+// ================================================
+
+
+// Handling Fetch with .then/.catch
+
+// const fetchData = fetch('https://randmuser.me/')
+// fetchData
+// .then((data)=>(console.log(data)))
+// .catch((error)=>(console.log('The Generated Error is:', error)))
+
+
+
+// ================================================
+
+// Handling Fetch with async/await
+
+// async function generatedData() {
+//     const fetchData = await fetch('https://randomuser.me/api')
+//     const data = await fetchData.json()
+//     console.log(data)
+// }
+// generatedData()
